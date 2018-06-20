7861d95755cd5aa66481e097870ad95c46f4a609
Gary Boyd <eponick@gmail.com>
2018-06-20 15:06:52 -0400

    Test get_size is hash, fail

--- test_pizza_app.rb
+++ test_pizza_app.rb
@@ -5,4 +5,8 @@ class TestPizzaApp < Minitest::Test
     def test_assert_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_size_is_hash
+        assert_equal(Hash, get_sizes().class)
+    end
 end
